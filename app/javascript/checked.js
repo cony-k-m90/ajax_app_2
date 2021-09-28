bd17c1bcd131fc5360167b0cacb3e3ad98e6103b
cony-k-m90 <78285637+cony-k-m90@users.noreply.github.com>
2021-09-28 17:48:27 +0900

    既読機能のフロントエンドの実装後、無駄な記述を削除

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,43 +1,8 @@
-// 1.checkという名前で関数を定義しましょう。
-//  DOMの取得からエンドポイントへのリクエストなどは、すべてこのcheck関数へ記述することにします。
-//  window.addEventListener("load", check);で、
-//  window（ページ）をload（読み込み）した時に実行します。
-//////////////////////////////////////////////////
-// function check() {}
-// window.addEventListener("load", check);
-//////////////////////////////////////////////////
-
-// 2.checked.jsに、クリック時のイベントを定義しましょう
-//  [index.html.erb]7行目〜14行目がメモ部分のブロックになります。
-//  この部分を「クリックした時」にイベントを発火させたいので、
-//  クリックされる部分の要素を取得するためにchecked.jsへ下記のように記述します。
-//  querySelectorAllメソッドで、postをクラス名にもつ要素を取得できます。
-//  postというクラス名を持つ要素はメモの数だけ存在します。
-//////////////////////////////////////////////////
-// function check() {
-//   const posts = document.querySelectorAll(".post");
-// }
-// window.addEventListener("load", check);
-//////////////////////////////////////////////////
-// 3.checked.jsに、「メモクリックのイベント時」に動作する処理を記述しましょう
-//  要素1つずつに対して、「クリック」した際に動作する処理を記述します。
-//  まずは、forEachを記述して、それぞれの要素への処理を記述する場所を用意します。
-//////////////////////////////////////////////////
-// function check() {
-//   const posts = document.querySelectorAll(".post");
-//   posts.forEach(function (post) {});
-// }
-// window.addEventListener("load", check);
-//////////////////////////////////////////////////
-//  次に処理としてaddEventListenerメソッドを使用し、引数にclickの指定をします。
-// これで、「要素1つずつに対して、
-// 『クリック』した際に動作するイベント駆動」を設定することができました。
-
 function check() {
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post) {
     if (post.getAttribute("data-load") != null) {
-      return null; //return null; によってJavaScriptの処理から抜け出せる
+      return null;
     }
     post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
@@ -49,7 +14,7 @@ function check() {
       XHR.onload = () => {
         if (XHR.status != 200) {
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
-          return null; //return null; によってJavaScriptの処理から抜け出せる
+          return null;
         }
         const item = XHR.response.post;
         if (item.checked === true) {
@@ -61,4 +26,4 @@ function check() {
     });
   });
 }
-setInterval(check, 1000); //setInterval は一定時間ごとに特定の処理を繰り返す
+setInterval(check, 1000);
